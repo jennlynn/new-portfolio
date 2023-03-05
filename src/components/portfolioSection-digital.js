0254d0c9f2463a063c9a5e802574f3cfe45336ae
Jennifer Rabin <jenniferrabin@MacBook-Pro-14-inch-2092.local>
2023-03-05 14:50:46 -0500

    new images, and color cleanup

--- src/components/portfolioSection-digital.js
+++ src/components/portfolioSection-digital.js
@@ -2,15 +2,16 @@ import React, { useState } from 'react';
 import "./portfolioSection.scss";
 import cx from 'classnames';
 import PortfolioItem from './portfolioItem';
-import PersonalSiteV1 from '../assets/images/portfolio-digital/site_v1-01.png';
+import PersonalSiteV1 from '../assets/images/portfolio-digital/site_v1-02.png';
 import TLPhotography from '../assets/images/portfolio-digital/TLPhotography.jpg';
 import CMBSTaiChi from '../assets/images/portfolio-digital/cmbs_taichi.jpg';
 import Firefly from '../assets/images/portfolio-digital/firefly1.jpg';
 import BfastClub from '../assets/images/portfolio-digital/BfastClub.jpg';
 import BUCDIANewsletter from '../assets/images/portfolio-digital/bucdia_newsletter.jpg';
 import FHC from '../assets/images/portfolio-digital/fhc.jpg';
-import JSP from '../assets/images/portfolio-digital/jsp.png';
-import CMBS2 from '../assets/images/portfolio-digital/cmbs_2.png';
+import JSP from '../assets/images/portfolio-digital/jsp_02.png';
+import CMBS2 from '../assets/images/portfolio-digital/cmbs_tai_chi_02.png';
+import HomeDash from '../assets/images/portfolio-digital/home_dash.png';
 
 const PortfolioSection = ({ name }) => {         
   const [isActive, setIsActive] = useState(false);
@@ -35,13 +36,14 @@ const PortfolioSection = ({ name }) => {
 
         <PortfolioItem src={PersonalSiteV1} name="V1 of this site" meta="web design & development" description="" />
         <PortfolioItem src={JSP} name="Johnson String Project" meta="web design & development" description="" />
-        <PortfolioItem src={CMBS2} name="CMBS v2" meta="Wix web design & development" description="" />
-        <PortfolioItem src={CMBSTaiChi} name="CMBS Tai Chi v1" meta="web design, development, wordpress implementation" description="" />
+        <PortfolioItem src={CMBS2} name="CMBS Tai Chi site v2" meta="Wix web design & development" description="" />
+        <PortfolioItem src={CMBSTaiChi} name="CMBS Tai Chi site v1" meta="web design, development, Wordpress implementation" description="" />
+        <PortfolioItem src={HomeDash} name="Hamoe Assistant Dashboard" meta="the banners were fun, but I might remove them soon, haha" description="" />
         <PortfolioItem src={FHC} name="Franciscan Hospital for Children website" meta="web design and prototype" description="desc" />
         <PortfolioItem src={Firefly} name="Firefly" meta="ad campaign design" description="" />
         <PortfolioItem src={BfastClub} name="the Breakfast Club diner" meta="brand identity design" description="" />
         <PortfolioItem src={BUCDIANewsletter} name="BUCDIA Newsletter" meta="newsletter layout and design" description="" />
-        <PortfolioItem src={TLPhotography} name="Tanya Lombardo Photography" meta="brand identity design" description="" />
+        {/* <PortfolioItem src={TLPhotography} name="Tanya Lombardo Photography" meta="brand identity design" description="" /> */}
 
         </div>
       </div>
